67966700bb0bbdd7d81a57e37ef485832717e35d
santiago120298 <santodechaves@hotmail.com>
2024-09-10 17:47:40 -0500

    10_09_24

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState(["goku", "tanjiro", "eren"]);
+  /*  const [isLoading, setIsLoading] = useState(false);
+  const handleClick = () => setIsLoading(!isLoading);
+
   const list = ["goku", "tanjiro", "eren"];
+  const handleSelect = (elemento: string) => {
+    console.log("imprimiendo", elemento);
+  };
   return (
     <Card>
       <CardBody title={"Hola mundo"} />
-      <List data={list} />
+      <List data={list} onSelect={handleSelect} />
+      <Button isLoading={isLoading} onClick={handleClick}>
+        hola mundo
+      </Button>
+    </Card>
+  );*/
+  const addMinions = () => setData([...data, "Minion"]);
+  const delMinions = () => setData(data.slice(0, -1));
+  return (
+    <Card>
+      <Button onClick={addMinions}>Agregar</Button>
+      <Button onClick={delMinions}>Eliminar</Button>
+      <List data={data} />
     </Card>
   );
 }
